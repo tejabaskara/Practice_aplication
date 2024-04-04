524fddb17da446c1300773ea8cd0ec6915dbdb62
tejabaskara <tejasidang@gmail.com>
2024-04-04 14:00:19 +0800

    update registration page(adding input text for password and email)

--- lib/register.dart
+++ lib/register.dart
@@ -16,8 +16,8 @@ class _page2State extends State<page2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
+        body: ListView(children: [
+      Column(children: [
         Padding(
             padding: EdgeInsets.only(top: 64),
             child: Center(
@@ -47,6 +47,33 @@ class _page2State extends State<page2> {
           ),
         ),
         Padding(
+            padding: EdgeInsets.only(top: 50),
+            child: Center(
+                child: SizedBox(
+                    width: 276,
+                    height: 40,
+                    child: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Name',
+                        fillColor: Color(0xffD9D9D9),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              BorderSide(color: Colors.blueGrey, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red, width: 1),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        print(nameController.text);
+                      },
+                    )))),
+        Padding(
             padding: EdgeInsets.only(top: 20),
             child: Center(
                 child: SizedBox(
@@ -56,24 +83,93 @@ class _page2State extends State<page2> {
                       controller: emailController,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: 'Name',
+                        labelText: 'Email',
                         fillColor: Color(0xffD9D9D9),
                         filled: true,
                         enabledBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
                           borderSide:
-                              BorderSide(color: Colors.blueGrey, width: 2),
+                              BorderSide(color: Colors.blueGrey, width: 1),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide(color: Colors.red, width: 2),
+                          borderSide: BorderSide(color: Colors.red, width: 1),
                         ),
                       ),
                       onChanged: (value) {
                         print(emailController.text);
                       },
                     )))),
-      ],
-    ));
+        Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Center(
+                child: SizedBox(
+                    width: 276,
+                    height: 40,
+                    child: TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        fillColor: Color(0xffD9D9D9),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              BorderSide(color: Colors.blueGrey, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red, width: 1),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        print(passwordController.text);
+                      },
+                    )))),
+        Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Center(
+                child: SizedBox(
+                    width: 276,
+                    height: 40,
+                    child: TextField(
+                      controller: confirmPasswordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Confirm Password',
+                        fillColor: Color(0xffD9D9D9),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              BorderSide(color: Colors.blueGrey, width: 1),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.red, width: 1),
+                        ),
+                      ),
+                      onChanged: (value) {
+                        print(confirmPasswordController.text);
+                      },
+                    )))),
+        Row(children: [
+          Padding(
+              padding: EdgeInsets.only(top: 30, left: 60),
+              child: Text(
+                "Do have an Account ?",
+                style: GoogleFonts.inter(
+                  textStyle: TextStyle(
+                    color: Color(0xff40B5AE),
+                    fontSize: 12,
+                  ),
+                ),
+              )),
+        ])
+      ])
+    ]));
   }
 }
