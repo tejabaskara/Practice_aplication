4c2304aaa72c9e728444b74be937b92fafabcc1c
tejabaskara <tejasidang@gmail.com>
2024-04-11 19:58:48 +0800

    update home page

--- lib/home.dart
+++ lib/home.dart
@@ -26,74 +26,229 @@ class _homePageState extends State<homePage> {
               )),
         ],
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Container(
-              // width: 280,
-              height: 280,
-              decoration: BoxDecoration(
-                color: Color(0xfffbe7c9),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/home_image.jpg'),
-                  fit: BoxFit.fitHeight,
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-              )),
-          Container(
-            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-            decoration: BoxDecoration(boxShadow: [
-              BoxShadow(
-                  color: Color(0xff101617).withOpacity(0.11),
-                  blurRadius: 40,
-                  spreadRadius: 0.0)
-            ]),
-            child: TextField(
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.all(15),
-                  hintText: 'What your insterest?',
-                  hintStyle: TextStyle(color: Color(0xffDDDADA), fontSize: 14),
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      borderSide: BorderSide.none)),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          Column(
             children: [
               Container(
-                margin: EdgeInsets.only(top: 20),
-                width: 150,
-                height: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.food_bank, size: 50, color: Color(0xff95D975)),
-                    Text('Food', style: GoogleFonts.inter(fontSize: 16)),
-                  ],
-                ),
-              ),
+                  // width: 280,
+                  height: 280,
+                  decoration: BoxDecoration(
+                    color: Color(0xfffbe7c9),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/home_image.jpg'),
+                      fit: BoxFit.fitHeight,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                  )),
               Container(
-                margin: EdgeInsets.only(top: 20),
-                width: 150,
-                height: 150,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.local_hospital,
-                        size: 50, color: Color(0xffFF0000)),
-                    Text('Health', style: GoogleFonts.inter(fontSize: 16)),
-                  ],
+                margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Color(0xff101617).withOpacity(0.11),
+                      blurRadius: 40,
+                      spreadRadius: 0.0)
+                ]),
+                child: TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.all(15),
+                      hintText: 'What your insterest?',
+                      hintStyle:
+                          TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+                      prefixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                          borderSide: BorderSide.none)),
                 ),
               ),
+              Row(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Container(
+                          width: 70,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            color: Color(0xff8FFF74),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Android",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ))),
+                  Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Container(
+                          width: 70,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            color: Color(0xff8FFF74),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Cooking",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ))),
+                  Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Container(
+                          width: 70,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            color: Color(0xff8FFF74),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Bussiness",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ))),
+                  Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Container(
+                          width: 70,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            color: Color(0xff8FFF74),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Design",
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ))),
+                ],
+              ),
+              courseBox("Course 1", "4.5"),
+              courseBox("Course 2", "3.0"),
             ],
-          ),
+          )
         ],
+      ),
+    );
+  }
+
+  Padding courseBox(String courseName, review) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10, top: 20, right: 10),
+      child: Container(
+        width: 300,
+        height: 150,
+        decoration: BoxDecoration(
+          color: Color(0xffD8FFC6),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Text(courseName,
+                    style: GoogleFonts.poppins(
+                      textStyle: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ))),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: 10,
+                    left: 10,
+                  ),
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      color: Color(0xffD9D9D9),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text('image',
+                          style: GoogleFonts.poppins(
+                            textStyle: TextStyle(
+                              color: Colors.black,
+                              fontSize: 10,
+                            ),
+                          )),
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 100),
+                          child: Icon(Icons.star, color: Colors.yellow),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Text(review,
+                              style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                  color: Color(0xff0066FF),
+                                  fontSize: 20,
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(
+                          top: 10,
+                          left: 10,
+                        ),
+                        child: Text(
+                            'Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\nPraesent vel. ',
+                            style: GoogleFonts.poppins(
+                              textStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                              ),
+                            ))),
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
