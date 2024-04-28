ff92e04b4ed3a218fc10e4de80a396d0fae094f2
tejabaskara <tejasidang@gmail.com>
2024-04-28 10:05:00 +0800

    finishing profile page and adding logout button

--- lib/profile.dart
+++ lib/profile.dart
@@ -17,40 +17,188 @@ class _profilePageState extends State<profilePage> {
 
   @override
   Widget build(BuildContext context) {
-    userInfo();
+    // userInfo();
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
+        title: Center(
+            child: Text("PROFILE",
+                style: GoogleFonts.inter(
+                    textStyle: TextStyle(color: Colors.black),
+                    fontWeight: FontWeight.bold))),
+        backgroundColor: Color(0xffcfe17c),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Text(
-              _storage.read('email'),
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
+          Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.only(top: 28),
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 100,
+                      backgroundImage:
+                          AssetImage('assets/images/profile_image.jpg'),
+                    ),
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(top: 50),
+                  child: Column(
+                    children: [
+                      Container(
+                          width: 280,
+                          height: 20,
+                          child: Text('Email',
+                              style: GoogleFonts.inter(
+                                textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ))),
+                      Center(
+                          child: Padding(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Container(
+                          width: 280,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Color(0xffD9D9D9),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                              padding: EdgeInsets.only(top: 5, left: 10),
+                              child: Text('hello@gmail.com',
+                                  style: GoogleFonts.inter(
+                                    textStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                    ),
+                                  ))),
+                        ),
+                      ))
+                    ],
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: [
+                      Container(
+                          width: 280,
+                          height: 20,
+                          child: Text('Name',
+                              style: GoogleFonts.inter(
+                                textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ))),
+                      Center(
+                          child: Padding(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Container(
+                          width: 280,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Color(0xffD9D9D9),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                              padding: EdgeInsets.only(top: 5, left: 10),
+                              child: Text('Niceguy',
+                                  style: GoogleFonts.inter(
+                                    textStyle: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                    ),
+                                  ))),
+                        ),
+                      ))
+                    ],
+                  )),
+              Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Column(
+                  children: [
+                    Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: ElevatedButton.icon(
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                      content: Text(
+                                          'This section is under development!')));
+                            },
+                            icon: Icon(
+                              Icons.edit,
+                              color: Colors.white,
+                            ),
+                            label: Text(
+                              "Edit Profile",
+                              style: GoogleFonts.inter(
+                                textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(160, 50),
+                              shape: ContinuousRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              backgroundColor: Colors.blue,
+                              // foregroundColor: Colors.black),
+                            ))),
+                    Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: ElevatedButton.icon(
+                            onPressed: () {
+                              logout();
+                            },
+                            icon: Icon(
+                              Icons.logout,
+                              color: Colors.white,
+                            ),
+                            label: Text(
+                              "Logout",
+                              style: GoogleFonts.inter(
+                                textStyle: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(160, 50),
+                              shape: ContinuousRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              backgroundColor: Colors.red,
+                              // foregroundColor: Colors.black),
+                            ))),
+                  ],
                 ),
               ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 20),
-            child: Text(
-              _storage.read('name'),
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ),
+            ],
+          )
         ],
       ),
     );
+  }
+
+  void logout() async {
+    try {
+      final _response = await _dio.get(
+        '${_apiUrl}/logout',
+        options: Options(
+          headers: {'Authorization': 'Bearer ${_storage.read('token')}'},
+        ),
+      );
+      print(_response.data);
+      _storage.erase();
+      Navigator.pushReplacementNamed(context, '/');
+    } on DioException catch (e) {
+      print('${e.response} - ${e.response?.statusCode}');
+    }
   }
 
   void userInfo() async {
