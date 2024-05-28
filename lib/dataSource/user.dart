cbb41d7fe91b8bf33bd095b976cf0f0811ec2b39
tejabaskara <tejasidang@gmail.com>
2024-05-28 18:23:14 +0800

    change user datasource and fixing dashboard

--- lib/dataSource/user.dart
+++ lib/dataSource/user.dart
@@ -1,67 +1,101 @@
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:tugas_login/component/showAlertDialog.dart';
 
 final _dio = Dio();
 final _storage = GetStorage();
 final _apiUrl = 'https://mobileapis.manpits.xyz/api';
 
-// untuk register user
-void registerUser(name, email, password, context) async {
+Future<void> registerUser(
+    TextEditingController formName,
+    TextEditingController formEmail,
+    TextEditingController formPassword,
+    TextEditingController formConfirmPassword,
+    BuildContext context) async {
+  if (formName.text.isEmpty ||
+      formEmail.text.isEmpty ||
+      formPassword.text.isEmpty ||
+      formConfirmPassword.text.isEmpty) {
+    showAlertDialog(context, "Error", "Harap mengisi setiap kolom data anda");
+    return;
+  }
+
+  if (!EmailValidator.validate(formEmail.text)) {
+    showAlertDialog(context, "Error", "Email tidak benar");
+    return;
+  }
+
+  if (formPassword.text != formConfirmPassword.text) {
+    showAlertDialog(context, "Error", "Password tidak sama");
+    return;
+  }
+
+  if (formPassword.text.length < 8) {
+    showAlertDialog(context, "Error", "Password minimal 8 karakter");
+    return;
+  }
+
   try {
-    final _register = await _dio.post(
+    final _response = await _dio.post(
       '${_apiUrl}/register',
-      data: {'name': name, 'email': email, 'password': password},
+      data: {
+        'name': formName.text,
+        'email': formEmail.text,
+        'password': formPassword.text
+      },
     );
-    final _login = await _dio.post(
-      '${_apiUrl}/login',
-      data: {'email': email, 'password': password},
-    );
-    _storage.write('token', _login.data['data']['token']);
-    print(_register.data);
-    print(_login.data);
-    final _userInfo = await _dio.get(
-      '${_apiUrl}/user',
-      options: Options(
-        headers: {'Authorization': 'Bearer ${_storage.read('token')}'},
-      ),
-    );
-    _storage.write('id', _userInfo.data['data']['user']['id']);
-    _storage.write('email', _userInfo.data['data']['user']['email']);
-    _storage.write('name', _userInfo.data['data']['user']['name']);
-    print(_storage.read('id'));
-    print(_storage.read('email'));
-    print(_storage.read('name'));
-    Navigator.pop(context);
-    Navigator.pushReplacementNamed(context, '/home');
+
+    if (!_response.data['success']) {
+      showAlertDialog(context, "Error", "${_response.data['message']}");
+      return;
+    }
+
+    loginUser(formEmail, formPassword, context);
   } on DioException catch (e) {
-    showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('close'),
-                ),
-              ],
-              title: const Text('Error'),
-              content: Text('${e.response?.data['message']}'),
-            ));
-    print('${e.response} - ${e.response?.statusCode}');
+    if (e.response != null && e.response!.statusCode! < 500) {
+      String errorMessage = "";
+      if (e.response!.data != null && e.response!.data['message'] != null) {
+        var message = e.response!.data['message'];
+        if (message is Map && message.isNotEmpty) {
+          var firstMessage = message.values.first;
+          if (firstMessage is List && firstMessage.isNotEmpty) {
+            errorMessage = firstMessage[0];
+          }
+        }
+      }
+      showAlertDialog(context, "Error", errorMessage);
+    } else {
+      showAlertDialog(context, "Error", "Internal Server Error");
+    }
+    return;
   }
 }
 
-//untuk login user
-void loginUser(email, password, context) async {
+Future<void> loginUser(TextEditingController formEmail,
+    TextEditingController formPassword, BuildContext context) async {
+  if (formEmail.text.isEmpty || formPassword.text.isEmpty) {
+    showAlertDialog(context, "Error", "Harap mengisi setiap kolom data anda");
+    return;
+  }
+
+  if (!EmailValidator.validate(formEmail.text)) {
+    showAlertDialog(context, "Error", "Format email yang Anda masukkan salah");
+    return;
+  }
+
   try {
-    final _response = await _dio.post(
-      '${_apiUrl}/login',
-      data: {'email': email, 'password': password},
-    );
-    print(_response.data);
+    final _response = await _dio.post('$_apiUrl/login',
+        data: {'email': formEmail.text, 'password': formPassword.text});
+
+    if (!_response.data['success']) {
+      showAlertDialog(context, "Error", "${_response.data['message']}");
+      return;
+    }
+
     _storage.write('token', _response.data['data']['token']);
+
     final _userInfo = await _dio.get(
       '${_apiUrl}/user',
       options: Options(
@@ -75,26 +109,112 @@ void loginUser(email, password, context) async {
     print(_storage.read('id'));
     print(_storage.read('email'));
     print(_storage.read('name'));
+
+    formEmail.clear();
+    formPassword.clear();
     Navigator.pop(context);
     Navigator.pushReplacementNamed(context, '/home');
+    return;
   } on DioException catch (e) {
-    showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: const Text('close'),
-                ),
-              ],
-              title: const Text('Error'),
-              content: Text('${e.response?.data['message']}'),
-            ));
-    print('${e.response} - ${e.response?.statusCode}');
+    if (e.response != null && e.response!.statusCode! < 500) {
+      showAlertDialog(
+          context, "Error", "Email atau Password yang Anda masukkan salah");
+    } else {
+      showAlertDialog(context, "Error", "Internal Server Error");
+    }
+    return;
   }
 }
+
+// untuk register user
+// void registerUser1(name, email, password, context) async {
+//   try {
+//     final _register = await _dio.post(
+//       '${_apiUrl}/register',
+//       data: {'name': name, 'email': email, 'password': password},
+//     );
+//     final _login = await _dio.post(
+//       '${_apiUrl}/login',
+//       data: {'email': email, 'password': password},
+//     );
+//     _storage.write('token', _login.data['data']['token']);
+//     print(_register.data);
+//     print(_login.data);
+//     final _userInfo = await _dio.get(
+//       '${_apiUrl}/user',
+//       options: Options(
+//         headers: {'Authorization': 'Bearer ${_storage.read('token')}'},
+//       ),
+//     );
+//     _storage.write('id', _userInfo.data['data']['user']['id']);
+//     _storage.write('email', _userInfo.data['data']['user']['email']);
+//     _storage.write('name', _userInfo.data['data']['user']['name']);
+//     print(_storage.read('id'));
+//     print(_storage.read('email'));
+//     print(_storage.read('name'));
+//     Navigator.pop(context);
+//     Navigator.pushReplacementNamed(context, '/home');
+//   } on DioException catch (e) {
+//     showDialog(
+//         context: context,
+//         builder: (context) => AlertDialog(
+//               actions: [
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                   child: const Text('close'),
+//                 ),
+//               ],
+//               title: const Text('Error'),
+//               content: Text('${e.response?.data['message']}'),
+//             ));
+//     print('${e.response} - ${e.response?.statusCode}');
+//   }
+// }
+
+// //untuk login user
+// void loginUser1(email, password, context) async {
+//   try {
+//     final _response = await _dio.post(
+//       '${_apiUrl}/login',
+//       data: {'email': email, 'password': password},
+//     );
+//     print(_response.data);
+//     _storage.write('token', _response.data['data']['token']);
+//     final _userInfo = await _dio.get(
+//       '${_apiUrl}/user',
+//       options: Options(
+//         headers: {'Authorization': 'Bearer ${_storage.read('token')}'},
+//       ),
+//     );
+//     print(_response.data);
+//     _storage.write('id', _userInfo.data['data']['user']['id']);
+//     _storage.write('email', _userInfo.data['data']['user']['email']);
+//     _storage.write('name', _userInfo.data['data']['user']['name']);
+//     print(_storage.read('id'));
+//     print(_storage.read('email'));
+//     print(_storage.read('name'));
+//     Navigator.pop(context);
+//     Navigator.pushReplacementNamed(context, '/home');
+//   } on DioException catch (e) {
+//     showDialog(
+//         context: context,
+//         builder: (context) => AlertDialog(
+//               actions: [
+//                 TextButton(
+//                   onPressed: () {
+//                     Navigator.of(context).pop();
+//                   },
+//                   child: const Text('close'),
+//                 ),
+//               ],
+//               title: const Text('Error'),
+//               content: Text('${e.response?.data['message']}'),
+//             ));
+//     print('${e.response} - ${e.response?.statusCode}');
+//   }
+// }
 
 //untuk logout
 void logoutUser(context) async {
--- lib/screen/dashboard/home.dart
+++ lib/screen/dashboard/home.dart
@@ -16,19 +16,6 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xfffbe7c9),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/profile');
-              },
-              icon: Icon(
-                Icons.account_circle,
-                size: 40,
-              )),
-        ],
-      ),
       bottomNavigationBar: _bottomNavigationBar(),
       body: ListView(
         children: [
@@ -36,13 +23,9 @@ class _homePageState extends State<homePage> {
             children: [
               Container(
                 width: double.infinity,
-                height: 280,
+                height: 110,
                 decoration: BoxDecoration(
                   color: Color(0xfffbe7c9),
-                  // image: DecorationImage(
-                  //   image: AssetImage('assets/images/home_image.jpg'),
-                  //   fit: BoxFit.fitHeight,
-                  // ),
                   borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(30),
                     bottomRight: Radius.circular(30),
@@ -52,52 +35,23 @@ class _homePageState extends State<homePage> {
                     padding: EdgeInsets.only(left: 20, top: 40),
                     child: Column(
                       children: [
-                        Row(
-                          children: [
-                            Text(
-                              "Hello ${_storage.read('name')}",
-                              style: GoogleFonts.poppins(
-                                textStyle: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 30,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: 20),
-                              child: Text(
-                                "Balances :",
-                                style: GoogleFonts.poppins(
+                        SizedBox(height: 10),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Hello ${_storage.read('name')}",
+                                style: GoogleFonts.inter(
                                   textStyle: TextStyle(
                                     color: Colors.black,
-                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 30,
                                   ),
                                 ),
                               ),
-                            )
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Icon(Icons.currency_pound_rounded,
-                                color: Colors.green),
-                            Padding(
-                              padding: EdgeInsets.only(left: 10),
-                              child: Text(
-                                "999.999.999",
-                                style: GoogleFonts.poppins(
-                                  textStyle: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
+                            ],
+                          ),
                         ),
                       ],
                     )),
@@ -105,96 +59,6 @@ class _homePageState extends State<homePage> {
             ],
           )
         ],
-      ),
-    );
-  }
-
-  Padding courseBox(String courseName, review) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10, top: 20, right: 10),
-      child: Container(
-        width: 300,
-        height: 150,
-        decoration: BoxDecoration(
-          color: Color(0xffD8FFC6),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          children: [
-            Padding(
-                padding: EdgeInsets.only(top: 10),
-                child: Text(courseName,
-                    style: GoogleFonts.poppins(
-                      textStyle: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ))),
-            Row(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                  ),
-                  child: Container(
-                    width: 80,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Color(0xffD9D9D9),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Center(
-                      child: Text('image',
-                          style: GoogleFonts.poppins(
-                            textStyle: TextStyle(
-                              color: Colors.black,
-                              fontSize: 10,
-                            ),
-                          )),
-                    ),
-                  ),
-                ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(left: 100),
-                          child: Icon(Icons.star, color: Colors.yellow),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 10),
-                          child: Text(review,
-                              style: GoogleFonts.poppins(
-                                textStyle: TextStyle(
-                                  color: Color(0xff0066FF),
-                                  fontSize: 20,
-                                ),
-                              )),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                        padding: EdgeInsets.only(
-                          top: 10,
-                          left: 10,
-                        ),
-                        child: Text(
-                            'Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\nPraesent vel. ',
-                            style: GoogleFonts.poppins(
-                              textStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: 12,
-                              ),
-                            ))),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -207,13 +71,9 @@ class _homePageState extends State<homePage> {
       ),
       BottomNavigationBarItem(
         icon: Icon(Icons.person),
-        label: 'List Anggota',
+        label: 'Profile',
       ),
-      BottomNavigationBarItem(
-        icon: Icon(Icons.money),
-        label: 'List Tabungan',
-      ),
-    ], selectedItemColor: Colors.amber, onTap: _onTap);
+    ], selectedItemColor: Colors.blue, onTap: _onTap);
   }
 
   _onTap(int tabIndex) {
@@ -222,12 +82,7 @@ class _homePageState extends State<homePage> {
         Navigator.pushNamed(context, '/home');
         break;
       case 1:
-        Navigator.pushNamed(context, "/anggota");
-        break;
-      case 2:
-        getBanyakAnggota();
-        iterationSaldo();
-        Navigator.pushNamed(context, "/tabungan");
+        Navigator.pushNamed(context, '/profile');
         break;
     }
   }
